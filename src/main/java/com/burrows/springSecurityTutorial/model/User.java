cc67b3cff6ea946af93041f865e00f69ae42a726
burrows99 <raunakburrows@gmail.com>
2022-02-11 22:49:29 -0800

    Updated spring security tutorial

--- src/main/java/com/burrows/springSecurityTutorial/model/User.java
+++ src/main/java/com/burrows/springSecurityTutorial/model/User.java
@@ -1,47 +1,30 @@
 package com.burrows.springSecurityTutorial.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
     String username;
+
+    @NotNull
+    @Column
     String password;
+
+    @NotNull
+    @Column
     String email;
+
+    @Column
     String role;
-
-    public User(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.role = role;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
--- src/main/java/com/burrows/springSecurityTutorial/service/UserService.java
+++ src/main/java/com/burrows/springSecurityTutorial/service/UserService.java
@@ -1,7 +1,9 @@
 package com.burrows.springSecurityTutorial.service;
 
 import com.burrows.springSecurityTutorial.model.User;
+import com.burrows.springSecurityTutorial.repository.UserRepository;
 import org.apache.catalina.LifecycleState;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,24 +11,30 @@ import java.util.List;
 
 @Service
 public class UserService {
-    List<User> list = new ArrayList<>();
+//    List<User> list = new ArrayList<>();
+//
+//    public UserService(){
+//        list.add(new User("abc","abc","abc@gmail.com"));
+//        list.add(new User("def","def","def@gmail.com"));
+//        list.add(new User("ghi","ghi","ghi@gmail.com"));
+//    }
 
-    public UserService(){
-        list.add(new User("abc","abc","abc@gmail.com"));
-        list.add(new User("def","def","def@gmail.com"));
-        list.add(new User("ghi","ghi","ghi@gmail.com"));
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     public List<User> getAllUsers(){
-        return this.list;
+//        return this.list;
+        return this.userRepository.findAll();
     }
 
     public User getUser(String username){
-        return this.list.stream().filter((user) -> user.getUsername().equals(username)).findAny().orElse(null);
+//        return this.list.stream().filter((user) -> user.getUsername().equals(username)).findAny().orElse(null);
+        return (User) this.userRepository.getById(username);
     }
 
     public User addUser(User user){
-        this.list.add(user);
-        return user;
+//        this.list.add(user);
+//        return user;
+        return (User) this.userRepository.save(user);
     }
 }
